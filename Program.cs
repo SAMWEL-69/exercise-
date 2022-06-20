34a1128e58efbdbd8bc44cf6aa0b09822c36e6d0
SAMWEL-69 <samwelngari032@gmail.com>
2022-06-20 19:39:51 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+ string inputStr="";
+ Console.WriteLine("Do While ()Loop");
+ do{
+    inputStr=Console.ReadLine();
+    Console.WriteLine("you entered:{0},inputStr");
+ }
+    while(inputStr!="exit");
+
+
+
+
+
+
